c1474a6c5ac923c66add0cc3ba2b72f8031ce499
HoshinoTented <limbolrain@gmail.com>
2018-11-30 22:26:49 +0800

    QAQ

--- judge.kts
+++ judge.kts
@@ -4,13 +4,6 @@ import java.io.*
 import java.nio.file.Paths
 import java.util.Scanner
 
-val pid = args.getOrNull(0) ?: throw RuntimeException("Please input problem id")
-val prefix = args.getOrNull(1) ?: "."
-val projectName = args.getOrNull(2) ?: throw RuntimeException("Unknown Project Name")
-val testDir = File("$prefix/test/$pid/")
-val executable = "$prefix/build/$projectName"
-val runtime : Runtime = Runtime.getRuntime()
-
 class JudgeException(val problem : String, msg : String) : Exception("Problem $problem : $msg")
 
 fun process(id : String, input : String, output : String) : Long {
@@ -57,14 +50,37 @@ fun process(id : String, input : String, output : String) : Long {
 	return end - start
 }
 
+fun loadParams() : Map<String, String> {
+	return args.map {
+		it.split('=').run {
+			get(0) to get(1)
+		}
+	}.toMap()
+}
+
+val config : Map<String, String> = loadParams()
+
+val pid = config["problem"] ?: throw IllegalArgumentException("Please input problem id, use param '-p <problem id>'")
+val prefix = config["prefix"] ?: throw IllegalArgumentException("Please input path's prefix, use param '-P <prefix>'")
+val projectName = config["name"] ?: throw IllegalArgumentException("Please input project name, use param '-n <name>'")
+val inFileName = config["in"] ?: "in.txt"
+val outFileName = config["out"] ?: "out.txt"
+val testDir = File("$prefix/test/$pid/")
+val executable = "$prefix/build/$projectName"
+val runtime : Runtime = Runtime.getRuntime()
+
+
 if (! testDir.exists()) throw JudgeException(pid, "No found test data")
 
 testDir.listFiles()?.forEach {
 	if (it.isDirectory) {
 		println("Testing ${it.name}...")
 
-		val input = Paths.get(it.path, "in.txt").toAbsolutePath().toFile().readText()
-		val output = Paths.get(it.path, "out.txt").toAbsolutePath().toFile().readText()
+		val inFile = Paths.get(it.path, inFileName).toFile().takeIf(File::exists) ?: Paths.get(it.path, "testdata.in").toFile().takeIf(File::exists) ?: throw JudgeException(pid, "Not found input test data")
+		val outFile = Paths.get(it.path, outFileName).toFile().takeIf(File::exists) ?: Paths.get(it.path, "testdata.out").toFile().takeIf(File::exists) ?: throw JudgeException(pid, "Not found output test data")
+
+		val input = inFile.readText()
+		val output = outFile.readText()
 
 		println("use ${process(pid, input, output)} ms")
 		println("Accepted!!")
